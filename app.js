4c903883b5484cf125d35f194386aae120548a25
miriadaxdani <danivectorsf@gmail.com>
2015-08-08 18:04:41 +0200

    Mod9 - Ejercicio P2P - Añadir auto-logout de sesion
    Primera parte del ejercicio obligatorio.
    Añadir auto-logout de sesión

--- app.js
+++ app.js
@@ -33,14 +33,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Helpers dinamicos:
 app.use(function(req, res, next) {
 	//guardar el path en session.redir para redireccionar a la vista anterior
+	
+	//inicializo el path de la sesion, para corregir el bug localhost:5000/login Ya que al logarse, antes se producia un error que no encontraba el path
+	if(!req.session.redir) req.session.redir="/";
 	if(!req.path.match(/\/login|\/logout/)) {
 		req.session.redir=req.path;
 	}
 	
 	// Hacer visible la session en las vistas
-	res.locals.session = req.session;
+	res.locals.session = req.session;	
 	next();
+});
 
+// Gestion de la duracion de la session. Si el servidor tarda mas de 2 minutos en recibir una peticion http, la session caduca.
+// NOTA: Tenemos un redirect automatico en login y logout. Por ejemplo, cuando clickamos en logout, y pasamos por los middlewares de apps, seguimos
+// manteniendo la session, aun no se ha destruido. Pero como en logout hacemos una redireccion al recurso anterior, entonces volvemos a pasar
+// por los middlewares de apps pero esta vez con la session destruida.    
+app.use(function(req, res, next) {
+	//La seteamos a false para que no vuelve a pintarla en la pagina.
+	if(req.session.sessionCaducada) req.session.sessionCaducada=false;
+	
+	//gestiona la caducidad de la session
+	if(req.session.user) {
+		var tiempoAct = new Date().getTime();
+		if(req.session.tiempo) {
+			var diferenciaTiempo = Math.floor((tiempoAct-req.session.tiempo)/1000); //resultado en segundos
+			if(diferenciaTiempo > 120) {
+				delete req.session.user; 
+				delete req.session.tiempo;
+				//para mostrar una alerta en la pagina
+				req.session.sessionCaducada=true;
+			} else { 
+				req.session.tiempo = tiempoAct;
+				//Si hacemos logout, quitamos la variable tiempo de la session. Para que el tiempo deje de contar.
+				if(req.path.match(/\/logout/)) delete req.session.tiempo;
+			}
+		}else req.session.tiempo = tiempoAct;
+	}
+	next();
 });
 
 app.use('/', routes); 
